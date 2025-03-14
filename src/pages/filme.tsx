08c3e90911d38942abc5b6ebd17f50c41e5a93f5
xezzz <pschaper18@gmail.com>
2025-03-14 02:34:46 +0100

    Minor adjustments

--- src/pages/filme.tsx
+++ src/pages/filme.tsx
@@ -166,7 +166,7 @@ export default function Home({
                         type="search"
                         id="default-search"
                         className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-                        placeholder="Search Spider Man"
+                        placeholder="Suche nach einem Film"
                         value={query}
                         onChange={(e) => {
                           e.preventDefault();
